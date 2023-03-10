fd0094999e5f7f97181a90473ba38a20c01b0f9f
Miguel Cobian <miguelcobian99@gmail.com>
2023-03-10 17:39:09 -0600

    app.js: Función de MostrarAlerta agregada; para crear un mensaje visual de alerta generando HTML

--- js/app.js
+++ js/app.js
@@ -13,11 +13,21 @@ document.addEventListener('DOMContentLoaded', function () {
     //Funciones
     function validar (e) {
         if (e.target.value.trim() === '') {
-            console.log('Vacio');
+            mostrarAlerta();
             
         } else {
             console.log('Hay algo');
         }
+        
+    }
+
+    function mostrarAlerta() {
+        //Generar alerta en HTML
+        const error = document.createElement('P'); //Creamos un Parrafo
+        error.textContent = 'Hubo un error'; // LE dimos este valor al Parrafo
+        error.
+
+        console.log(error);
         
     }
     
