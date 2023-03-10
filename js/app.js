61e69582a8a9ccb9020e749cce224af17f051780
Miguel Cobian <miguelcobian99@gmail.com>
2023-03-10 17:06:51 -0600

    app.js: Elementos input del DOM seleccionados

---
+++ js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    //Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+
+    
+    console.log(inputMensaje);
+    
+
+})
