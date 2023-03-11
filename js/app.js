0d14bd06057fe21604d354eb7ee8f66bce5802bf
Miguel Cobian <miguelcobian99@gmail.com>
2023-03-10 18:59:03 -0600

    Agregamos funcionalidad para eliminar alertas cuando se pone algo

--- js/app.js
+++ js/app.js
@@ -14,32 +14,34 @@ document.addEventListener('DOMContentLoaded', function () {
     //Funciones
     function validar (e) {
         console.log(e.target.parentElement);
-        
-        
+
         if (e.target.value.trim() === '') {
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-            
-        } else {
-            console.log('Hay algo');
-        }
+            return;
+        } 
         
+        limpiarAlerta(e.target.parentElement);
     }
 
     function mostrarAlerta(mensaje, referencia) {
+        limpiarAlerta(referencia);
+        
+        //Generar alerta en HTML
+        const error = document.createElement('P'); //Creamos un Parrafo
+        error.textContent = mensaje; // 
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center'); //Clases para estilos de texto en Tailwind
+
+        //Inyectar el Error al Formulario
+        referencia.appendChild(error);
+    }
+
+    function limpiarAlerta(referencia) {
         //Comprueba si ya existe una alerta
         const alerta = referencia.querySelector('.bg-red-600');//Busca en el div que seleccione si hay bg-red-600
 
         if(alerta) {
             alerta.remove();
         }
-        
-        //Generar alerta en HTML
-        const error = document.createElement('P'); //Creamos un Parrafo
-        error.textContent = mensaje; // LE dimos este valor al Parrafo
-        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center'); //Clases para estilos de texto en Tailwind
-
-        //Inyectar el Error al Formulario
-        referencia.appendChild(error);
     }
     
 
