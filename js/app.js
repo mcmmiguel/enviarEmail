24b26edb63f33ceb7d08866a7dd575885e4a8aa1
Miguel Cobian <miguelcobian99@gmail.com>
2023-03-10 19:45:11 -0600

    Funcionalidad para habilitar o deshabilitar boton de enviar

--- js/app.js
+++ js/app.js
@@ -13,11 +13,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
 
     //Asignar eventos
-    inputEmail.addEventListener('blur', validar)
-    inputAsunto.addEventListener('blur', validar);
-    inputMensaje.addEventListener('blur', validar);
+    inputEmail.addEventListener('input', validar)
+    inputAsunto.addEventListener('input', validar);
+    inputMensaje.addEventListener('input', validar);
 
     //Funciones
     function validar (e) {
@@ -25,10 +26,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (e.target.value.trim() === '') {
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            email [e.target.id] = '';
+            comprobarEmail();
             return;
         } 
         if (e.target.id === 'email' && !validarEmail(e.target.value)) {
             mostrarAlerta(`El email no es válido`, e.target.parentElement);
+            email [e.target.id] = '';
+            comprobarEmail();
             return;
         }
         limpiarAlerta(e.target.parentElement);
@@ -71,10 +76,17 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function comprobarEmail() {
-        console.log(Object.values(email).includes(''));
+        console.log(email);
         
-        
-        console.log('Email');
+         if(Object.values(email).includes('')) {
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return;
+         } 
+
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disabled = false;
+         
         
     }
 
